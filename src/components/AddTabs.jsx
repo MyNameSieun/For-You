a6b30dfeee55f69529b19d21802ea27020200608
박시은 <charm516@naver.com>
2024-09-07 23:40:48 +0900

    [feat] - 쿼리 파라미터 적용

--- src/components/AddTabs.jsx
+++ src/components/AddTabs.jsx
@@ -1,4 +1,5 @@
 import { useSelected } from 'context/SelectedContext';
+import { useSearchParams } from 'react-router-dom';
 import styled, { css } from 'styled-components';
 
 const AddTabs = () => {
@@ -6,12 +7,26 @@ const AddTabs = () => {
 
   const characters = ['토토로', '키키', '포뇨', '치히로', '소피', '가오나시'];
 
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  // 쿼리 파라미터 설정
+  const updateSearchParams = (value) => {
+    setSearchParams({ selected: value });
+  };
+
   return (
     <div>
       <StAddTabsTitle>누구에게 보내실 건가요?</StAddTabsTitle>
       <StTabList>
         {characters.map((character, index) => (
-          <StTabItem key={index} $isSelected={character === selected} onClick={() => setSelected(character)}>
+          <StTabItem
+            key={index}
+            $isSelected={character === selected}
+            onClick={() => {
+              setSelected(character);
+              updateSearchParams(character);
+            }}
+          >
             {character}
           </StTabItem>
         ))}
